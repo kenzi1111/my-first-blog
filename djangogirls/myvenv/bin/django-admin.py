a8461b6aa3fba5a473863a99d7a32a883e360533
Kenzi <you@example.com>
2021-10-06 22:26:16 +0900

    My Django Girls app, first commit

---
+++ djangogirls/myvenv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/oonishikousei/Desktop/djangogirls/djangogirls/myvenv/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
